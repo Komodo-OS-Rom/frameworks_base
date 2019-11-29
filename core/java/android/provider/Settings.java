5d8a610bf7d66a418197d80060030201b82c8fc8
ezio84 <brabus84@gmail.com>
2019-11-29 23:46:55 +0100

    Screenrecord: Remember last recording options
    Change-Id: Iaeb4dab6c15c57e91767c3741018ef88fb02c604

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5310,6 +5310,37 @@ public final class Settings {
                 "double_tap_sleep_lockscreen";
 
         /**
+         * Screenrecord: enable mic
+         * @hide
+         */
+        public static final String SCREENRECORD_ENABLE_MIC = "screenrecord_enable_mic";
+
+        /**
+         * Screenrecord: show taps
+         * @hide
+         */
+        public static final String SCREENRECORD_SHOW_TAPS = "screenrecord_show_taps";
+
+        /**
+         * Screenrecord: show stop dot
+         * @hide
+         */
+        public static final String SCREENRECORD_STOP_DOT = "screenrecord_stop_dot";
+
+        /**
+         * Screenrecord: low quality
+         * @hide
+         */
+        public static final String SCREENRECORD_LOW_QUALITY = "screenrecord_low_quality";
+
+        /**
+         * IMPORTANT: If you add a new public settings you also have to add it to
+         * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
+         * it to PRIVATE_SETTINGS below. Also add a validator that can validate
+         * the setting value. See an example above.
+         */
+
+        /**
          *  Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
