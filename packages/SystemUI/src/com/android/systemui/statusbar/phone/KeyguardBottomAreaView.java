47bd3d6e97e77e145603f081e31e4ba4b93e6b7f
Mikazuuu07 <mikazuuu07@nusantararom.org>
2022-05-25 01:50:18 +0000

    Revert "fwb: Guard some functions against face unlock"
    This reverts commit c89a1b7d21543313ded58288f5a13f8d73f0eb09.
    
    Signed-off-by: Mikazuuu07 <mikazuuu07@nusantararom.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -883,15 +883,6 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                     updateCameraVisibility();
                     updateLeftAffordance();
                 }
-
-                @Override
-                public void onBiometricRunningStateChanged(boolean running,
-                            BiometricSourceType biometricSourceType) {
-                    if (biometricSourceType == BiometricSourceType.FINGERPRINT) {
-                        mIsFingerprintRunning = running;
-                        updateIndicationAreaPadding();
-                    }
-                }
             };
 
     public void updateLeftAffordance() {
